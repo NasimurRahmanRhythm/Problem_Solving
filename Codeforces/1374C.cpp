cba0f589fa20778b04daaacd824254dd0f9d8fe3
NasimurRahmanRhythm <76849831+NasimurRahmanRhythm@users.noreply.github.com>
2022-04-03 08:52:42 +0600

    Add files via upload

---
+++ Codeforces/1374C.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,n)             for(ll i=1;i<=n;i++)
+#define LOOP(i,n)            for(ll i=0;i<n;i++)
+#define loop(a,b)            for(ll i=a;i<=b;i++)
+#define sci(n)               scanf("%d",&n)
+#define scl(n)               scanf("%lld",&n)
+#define scl3(a,b,c)          scanf("%lld%lld%lld",&a,&b,&c);
+#define scl2(a,b)            scanf("%lld%lld",&a,&b)
+#define scd(n)               scanf("%lf",&n)
+#define srt(g)               sort(g.begin(),g.end())
+#define rvs(g)               reverse(g.begin(),g.end())
+#define pl                   pair<long long,long long>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pi acos(-1.0)
+#define f first
+#define s second
+int main()
+{
+    ll t;
+    scl(t);
+    while(t--)
+    {
+        ll n;
+        string s;
+        cin >> n >> s;
+        stack<char>st;
+        LOOP(i,s.size())
+        {
+            if(s[i]=='(')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if(!st.empty())
+                {
+                    st.pop();
+                }
+            }
+        }
+        cout << st.size() << "\n";
+    }
+}
+
