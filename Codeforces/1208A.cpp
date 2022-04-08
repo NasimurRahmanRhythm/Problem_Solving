3dea0d16888ec9ffb510fe56b98ccf72b78b442b
Nasimur Rahman Rhythm <76849831+NasimurRahmanRhythm@users.noreply.github.com>
2022-04-08 22:14:51 +0600

    Add files via upload

---
+++ Codeforces/1208A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,n)             for(ll i=1;i<=n;i++)
+#define LOOP(i,n)            for(ll i=0;i<n;i++)
+#define loop(a,b)            for(ll i=a;i<=b;i++)
+#define sci(n)               scanf("%d",&n)
+#define scl(n)               scanf("%lld",&n)
+#define scd(n)               scanf("%lf",&n)
+#define srt(g)               sort(g.begin(),g.end())
+#define rvs(g)               reverse(g.begin(),g.end())
+#define pl                   pair<long long,long long>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+int main()
+{
+    ll t;
+    scl(t);
+    while(t--)
+    {
+        ll a[5],n;
+        cin >> a[0] >> a[1] >> n;
+        a[2]=a[0]^a[1];
+        cout << a[n%3] << "\n";
+    }
+}
+
