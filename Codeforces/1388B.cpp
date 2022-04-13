8a78e9d6cdce5a0dfa6249f2373102101c15c23a
Nasimur Rahman Rhythm <76849831+NasimurRahmanRhythm@users.noreply.github.com>
2022-04-13 23:23:26 +0600

    Add files via upload

---
+++ Codeforces/1388B.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,n)             for(ll i=1;i<=n;i++)
+#define LOOP(i,n)            for(ll i=0;i<n;i++)
+#define loop(a,b)            for(ll i=a;i<=b;i++)
+#define sci(n)               scanf("%d",&n)
+#define scl(n)               scanf("%lld",&n)
+#define scl3(a,b,c)          scanf("%lld%lld%lld",&a,&b,&c);
+#define scl2(a,b)            scanf("%lld%lld",&a,&b)
+#define scd(n)               scanf("%lf",&n)
+#define srt(g)               sort(g.begin(),g.end())
+#define rvs(g)               reverse(g.begin(),g.end())
+#define pl                   pair<long long,long long>
+#define pll                  pair<long long,pair<long long,long long>>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pi acos(-1.0)
+#define f first
+#define s second
+int main()
+{
+    ll t;
+    scl(t);
+    while(t--)
+    {
+        ll n;
+        scl(n);
+        ll p=n/4;
+        if(n%4)p++;
+        for(ll i=1;i<=n-p;i++)
+        {
+            cout << "9";
+        }
+        for(ll i=1;i<=p;i++)
+        {
+            cout << "8";
+        }
+        cout << "\n";
+    }
+}
+
