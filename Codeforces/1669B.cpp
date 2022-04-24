2b87960b20188951ac2efc4f9628bb3167a2d83f
Nasimur Rahman Rhythm <76849831+NasimurRahmanRhythm@users.noreply.github.com>
2022-04-24 22:48:59 +0600

    Add files via upload

---
+++ Codeforces/1669B.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,n)             for(ll i=1;i<=n;i++)
+#define LOOP(i,n)            for(ll i=0;i<n;i++)
+#define loop(a,b)            for(ll i=a;i<=b;i++)
+#define sci(n)               scanf("%d",&n)
+#define scl(n)               scanf("%lld",&n)
+#define scl3(a,b,c)          scanf("%lld%lld%lld",&a,&b,&c);
+#define scl2(a,b)            scanf("%lld%lld",&a,&b)
+#define scd(n)               scanf("%lf",&n)
+#define pf(n)                printf("%lld\n",n)
+#define srt(g)               sort(g.begin(),g.end())
+#define rvs(g)               reverse(g.begin(),g.end())
+#define pl                   pair<long long,long long>
+#define pll                  pair<long long,pair<long long,long long>>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pi acos(-1.0)
+#define f first
+#define s second
+int main()
+{
+    ll t;
+    scl(t);
+    while(t--)
+    {
+        ll n;
+        scl(n);
+        ll a[n+5];
+        map<ll,ll>mp;
+        LOOP(i,n)
+        {
+            scl(a[i]);
+            mp[a[i]]++;
+        }
+        ll flag=0;
+        LOOP(i,n)
+        {
+            if(mp[a[i]]>=3)
+            {
+                cout << a[i] << "\n";
+                flag=1;
+                break;
+            }
+        }
+        if(!flag)
+        {
+            cout << "-1\n";
+        }
+    }
+}
+
