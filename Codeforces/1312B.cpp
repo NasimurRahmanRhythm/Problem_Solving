1554c153d2dba1da75ad7befd41fcab8276635fa
Nasimur Rahman Rhythm <76849831+NasimurRahmanRhythm@users.noreply.github.com>
2022-06-15 21:02:27 +0600

    Add files via upload

---
+++ Codeforces/1312B.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define sci(n)               scanf("%d",&n)
+#define scl(n)               scanf("%lld",&n)
+#define scd(n)               scanf("%lf",&n)
+#define FOR(i,n)             for(ll i=1;i<=n;i++)
+#define LOOP(i,n)            for(ll i=0;i<n;i++)
+#define loop(a,b)            for(ll i=a;i<=b;i++)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+int main()
+{
+    ll t;
+    scl(t);
+    while(t--)
+    {
+        ll n;
+        scl(n);
+        vector<ll>v;
+        LOOP(i,n)
+        {
+            ll x;
+            scl(x);
+            v.pb(x);
+        }
+        sort(v.begin(),v.end());
+        for(ll i=v.size()-1;i>=0;i--)
+        {
+            cout << v[i] << " ";
+        }
+        cout << "\n";
+    }
+}
+
