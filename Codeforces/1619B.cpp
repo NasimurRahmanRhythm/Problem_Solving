2e2f2a19086e19b0256cc3b54f5c149e8b946a90
Nasimur Rahman Rhythm <nasimurrahman4538@gmail.com>
2022-07-28 13:12:31 +0600

    Add files via upload

---
+++ Codeforces/1619B.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,n)             for(ll i=1;i<=n;i++)
+#define LOOP(i,n)            for(ll i=0;i<n;i++)
+#define loop(a,b)            for(ll i=a;i<=b;i++)
+#define sci(n)               scanf("%d",&n)
+#define scl(n)               scanf("%lld",&n)
+#define scl3(a,b,c)          scanf("%lld%lld%lld",&a,&b,&c);
+#define scl2(a,b)            scanf("%lld%lld",&a,&b)
+#define scd(n)               scanf("%lf",&n)
+#define pf(n)                printf("%lld\n",n)
+#define srt(g)               sort(g.begin(),g.end())
+#define rvs(g)               reverse(g.begin(),g.end())
+#define pl                   pair<long long,long long>
+#define pll                  pair<long long,pair<long long,long long>>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pi acos(-1.0)
+#define f first
+#define s second
+int main()
+{
+    ll t;
+    scl(t);
+    while(t--)
+    {
+        ll n;
+        scl(n);
+        ll ans=0;
+        map<ll,ll>mp;
+        for(ll i=1;i*i<=n;i++)
+        {
+            if(mp[i*i]==0)
+            {
+                ans++;
+                mp[i*i]++;
+            }
+            if(i*i*i<=n && mp[i*i*i]==0)
+            {
+                ans++;
+                mp[i*i*i]++;
+            }
+        }
+        pf(ans);
+    }
+}
+
