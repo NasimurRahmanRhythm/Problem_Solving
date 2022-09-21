3814de2be4ef58bca112f73f2a365d9e7808f679
Nasimur Rahman Rhythm <nasimurrahman4538@gmail.com>
2022-09-21 21:20:15 +0600

    Add files via upload

---
+++ Codeforces/58A.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,n)             for(ll i=1;i<=n;i++)
+#define LOOP(i,n)            for(ll i=0;i<n;i++)
+#define loop(a,b)            for(ll i=a;i<=b;i++)
+#define sci(n)               scanf("%d",&n)
+#define scl(n)               scanf("%lld",&n)
+#define scl3(a,b,c)          scanf("%lld%lld%lld",&a,&b,&c);
+#define scl2(a,b)            scanf("%lld%lld",&a,&b)
+#define scd(n)               scanf("%lf",&n)
+#define pf(n)                printf("%lld\n",n)
+#define srt(g)               sort(g.begin(),g.end())
+#define rvs(g)               reverse(g.begin(),g.end())
+#define pl                   pair<long long,long long>
+#define pll                  pair<long long,pair<long long,long long>>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pi acos(-1.0)
+#define f first
+#define s second
+int main()
+{
+    string s;
+    string a="hello";
+    cin >> s;
+    ll k=0,flag=0;
+    LOOP(i,s.size())
+    {
+        if(s[i]==a[k])
+        {
+            k++;
+            if(k==5)
+            {
+                flag=1;
+                break;
+            }
+        }
+    }
+    if(flag)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+}
+
